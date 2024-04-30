c6d88265a0f7f05a5d7f1d7f5c73062c281c9347
AlexNg <contact@ngjx.org>
2024-04-30 12:01:33 +0800

    style: Conform to linting

--- appLB.py
+++ appLB.py
@@ -1,7 +1,7 @@
 import os
 import openai
 import chainlit as cl
-from pathlib import Path 
+from pathlib import Path
 import shutil
 
 from llama_index.core import (
@@ -21,7 +21,7 @@ from chainlit.input_widget import TextInput
 
 # openai.api_key = os.environ.get("OPENAI_API_KEY")
 openai.api_key = ""
-list_topics=["IT1111", "IT2222", "IT3333", "IT4444"]
+list_topics = ["IT1111", "IT2222", "IT3333", "IT4444"]
 try:
     # rebuild storage context
     storage_context = StorageContext.from_defaults(persist_dir="./prompt")
@@ -32,10 +32,11 @@ except:
     index = VectorStoreIndex.from_documents(documents)
     index.storage_context.persist("./prompt")
 
-current_topic=""
+current_topic = ""
+
 
 async def Updata_RAG_Topic(topic):
-    storage_context = StorageContext.from_defaults(persist_dir="./"+topic)
+    storage_context = StorageContext.from_defaults(persist_dir="./" + topic)
     # load index
     index = load_index_from_storage(storage_context)
     Settings.llm = OpenAI(
@@ -44,26 +45,27 @@ async def Updata_RAG_Topic(topic):
     Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
     Settings.context_window = 4096
 
-    service_context = ServiceContext.from_defaults(callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler()]))
-    query_engine = index.as_query_engine(streaming=True, similarity_top_k=2, service_context=service_context)
+    service_context = ServiceContext.from_defaults(
+        callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler()])
+    )
+    query_engine = index.as_query_engine(
+        streaming=True, similarity_top_k=2, service_context=service_context
+    )
     cl.user_session.set("query_engine", query_engine)
 
     await cl.Message(
-        author="Assistant", content="Hello! Im an AI assistant. How may I help you "+ topic +" information?"
+        author="Assistant",
+        content="Hello! Im an AI assistant. How may I help you "
+        + topic
+        + " information?",
     ).send()
-
-
 
 
 @cl.on_chat_start
 async def start():
-    actions = [
-        cl.Action(name="upload", value="example_value", description="Click me!")
-    ]
+    actions = [cl.Action(name="upload", value="example_value", description="Click me!")]
 
     await cl.Message(content="To upload new module content:", actions=actions).send()
-
-
 
     settings = await cl.ChatSettings(
         [
@@ -76,7 +78,7 @@ async def start():
         ]
     ).send()
     value = settings["Topics"]
-    global current_topic 
+    global current_topic
     current_topic = settings["Topics"]
 
     Settings.llm = OpenAI(
@@ -85,24 +87,30 @@ async def start():
     Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
     Settings.context_window = 4096
 
-    service_context = ServiceContext.from_defaults(callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler()]))
-    query_engine = index.as_query_engine(streaming=True, similarity_top_k=2, service_context=service_context)
+    service_context = ServiceContext.from_defaults(
+        callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler()])
+    )
+    query_engine = index.as_query_engine(
+        streaming=True, similarity_top_k=2, service_context=service_context
+    )
     cl.user_session.set("query_engine", query_engine)
 
-    topic = settings['Topics']
+    topic = settings["Topics"]
 
     await cl.Message(
-        author="Assistant-"+topic, content="Hello! Im an AI assistant. How may I help you?"
+        author="Assistant-" + topic,
+        content="Hello! Im an AI assistant. How may I help you?",
     ).send()
+
 
 @cl.on_settings_update
 async def setup_agent(settings):
     print("on_settings_update", settings)
-    topic = settings['Topics']
-    global current_topic 
+    topic = settings["Topics"]
+    global current_topic
     current_topic = settings["Topics"]
-    #change the RAG Topics
-    storage_context = StorageContext.from_defaults(persist_dir="./"+topic)
+    # change the RAG Topics
+    storage_context = StorageContext.from_defaults(persist_dir="./" + topic)
     # load index
     index = load_index_from_storage(storage_context)
     Settings.llm = OpenAI(
@@ -111,22 +119,29 @@ async def setup_agent(settings):
     Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
     Settings.context_window = 4096
 
-    service_context = ServiceContext.from_defaults(callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler()]))
-    query_engine = index.as_query_engine(streaming=True, similarity_top_k=2, service_context=service_context)
+    service_context = ServiceContext.from_defaults(
+        callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler()])
+    )
+    query_engine = index.as_query_engine(
+        streaming=True, similarity_top_k=2, service_context=service_context
+    )
     cl.user_session.set("query_engine", query_engine)
 
     await cl.Message(
-        author="Assistant-"+topic, content="Hello! Im an AI assistant. How may I help you "+ topic +" information?"
+        author="Assistant-" + topic,
+        content="Hello! Im an AI assistant. How may I help you "
+        + topic
+        + " information?",
     ).send()
 
 
 @cl.on_message
 async def main(message: cl.Message):
-    query_engine = cl.user_session.get("query_engine") # type: RetrieverQueryEngine
+    query_engine = cl.user_session.get("query_engine")  # type: RetrieverQueryEngine
 
-    global current_topic 
-   
-    msg = cl.Message(content="", author="Assistant-"+ current_topic)
+    global current_topic
+
+    msg = cl.Message(content="", author="Assistant-" + current_topic)
 
     res = await cl.make_async(query_engine.query)(message.content)
 
@@ -138,7 +153,7 @@ async def main(message: cl.Message):
 @cl.action_callback("upload")
 async def on_action(action: cl.Action):
     print("The user clicked on the action button!")
-     # Wait for the user to upload a PDF file
+    # Wait for the user to upload a PDF file
     files = await cl.AskFileMessage(
         content="Please upload a PDF file to begin!", accept=["application/pdf"]
     ).send()
@@ -146,15 +161,15 @@ async def on_action(action: cl.Action):
     pdf_file = files[0]
 
     dirname = os.path.dirname(pdf_file.path)
-    
-    filename= Path(pdf_file.name).stem 
+
+    filename = Path(pdf_file.name).stem
     print(dirname)
     print(filename)
     print(pdf_file.path)
 
-    #ask for the new module name, use to create dir
+    # ask for the new module name, use to create dir
     res = await cl.AskUserMessage(content="What is the module name?", timeout=30).send()
-    new_module = res['output']
+    new_module = res["output"]
     if res:
         await cl.Message(
             content=f"Your new module name is: {res['output']}",
@@ -172,32 +187,28 @@ async def on_action(action: cl.Action):
         ]
     ).send()
 
-
-    #create new dir then subdir data to store the source file generationg vector database
-    directory = "./"+new_module+"/data"
+    # create new dir then subdir data to store the source file generationg vector database
+    directory = "./" + new_module + "/data"
     try:
-      os.makedirs(directory)
-      print(f"Directory '{directory}' created successfully")
+        os.makedirs(directory)
+        print(f"Directory '{directory}' created successfully")
     except OSError as e:
-      print(f"Error: {e}")
+        print(f"Error: {e}")
 
-    #copy scr file to dest file
-    destination_file = directory+"/"+new_module+".pdf"
+    # copy scr file to dest file
+    destination_file = directory + "/" + new_module + ".pdf"
     shutil.copy2(pdf_file.path, destination_file)
 
-    #store into vector 
+    # store into vector
     create_documents = SimpleDirectoryReader(directory).load_data(show_progress=True)
     create_index = VectorStoreIndex.from_documents(create_documents)
-    create_index.storage_context.persist("./"+new_module)
-
+    create_index.storage_context.persist("./" + new_module)
 
     shutil.rmtree(dirname, ignore_errors=True)
 
     # You can then process the PDF file as needed
-    await cl.Message(
-        content=f"`{pdf_file.name}` uploaded successfully!"
-    ).send()
+    await cl.Message(content=f"`{pdf_file.name}` uploaded successfully!").send()
 
-    content=f"`{pdf_file.name}` uploaded successfully!"
+    content = f"`{pdf_file.name}` uploaded successfully!"
 
     return content
